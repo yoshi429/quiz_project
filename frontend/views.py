35b1cbbd004fc994fbfb0fa7e3979f373e72eac8
yoshi429 <1gk6204y@komazawa-u.ac.jp>
2021-09-05 00:55:42 +0900

    add detail

--- frontend/views.py
+++ frontend/views.py
@@ -3,13 +3,23 @@ from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
+    """
+    メイン画面
+    """
     template_name = 'frontend/main.html'
 
 
 def terms_of_service_view(request):
+    """
+    利用規約
+    """
     return render (request, 'components/terms_of_service.html')
 
+
 def page_not_found(request, exception):
+    """
+    404画面
+    """
     return render(request, 'components/404.html', status=404)
 
 
